ca3eb619bb987b67e343a36cb38b8d20317c548e
Eric Nguyen <erictn3@uci.edu>
2022-03-24 13:46:55 -0700

    return methods for app module

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ const APIcontroller = (function() {
     const data = await result.json();
     return data.access_token;
   }
+  // get list of genres
+  // this method receives token of parameters
+  // javascript template literals in api to allow expressions in string
 
   const _getGenres = async (token) => {
     const result = await fetch(`https://api.spotify.com/v1/browse/categoris?locale=sv_US`, {
@@ -28,6 +31,8 @@ const APIcontroller = (function() {
     return data.categories.items;
   }
 
+  // genreId is a parameter
+  // create a limit and genre id for specificity search
   const _getPlaylistByGenre = async (token, genreId) => {
 
     const limit = 10;
@@ -40,7 +45,9 @@ const APIcontroller = (function() {
     const data = await result.json();
     return data.playlists.items;
   }
-
+  // tracks endpoint parameter
+  // included in dataset retrieved when first pull playlist
+  // return items array object
   const _getTracks = async (token, tracksEndPoint) => {
 
     const limit = 10;
@@ -54,6 +61,9 @@ const APIcontroller = (function() {
     return data.items;
   }
 
+
+  // get actual track
+  // display all tracks and user will be able to specify single track
   const _getTrack = async (token, trackEndPoint) => {
 
     const result = await fetch(`${trackEndPoint}`, {
@@ -65,5 +75,25 @@ const APIcontroller = (function() {
     return data;
   } 
 
+  // return methods we want to expose to outside scope
+  // use closures for the publicly declared get token mthod has access to the privately implemented get token mthod
+  return {
+    getToken() {
+      return _getToken();
+    },
+    getGenres(token) {
+      return _getGenres(token);
+    },
+    getPlaylistByGenre(token, genreId) {
+      return _getPlaylistByGenre(token, genreId);
+    },
+    getTracks(token, tracksEndPoint) {
+      return _getTracks(token, tracksEndPoint);
+    },
+    getTrack(token, trackEndPoint) {
+      return _getTrack(token, trackEndPoint);
+    }
+  }
 
+  // UI module
 })();
